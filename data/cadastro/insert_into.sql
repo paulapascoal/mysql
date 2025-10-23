8459b02fb9487b227bc4d74c9ab0f9febc25a934
Paula Pascoal <paulaiasmimpascoal2012@gmail.com>
2025-10-23 11:13:30 -0300

    mysql aulas 003-005

---
+++ data/cadastro/insert_into.sql
@@ -0,0 +1,6 @@
+insert into pessoas values
+(DEFAULT, 'Ana', '1975-12-22', 'F', '52.3', '1.45', 'EUA'),
+(DEFAULT, 'Pedro', '2000-07-15', 'M', '52.3', '1.45', 'Brasil'),
+(DEFAULT, 'Maria', '1999-05-30', 'M', '75.9', '1.70', 'Portugual');
+
+select * from pessoas;
